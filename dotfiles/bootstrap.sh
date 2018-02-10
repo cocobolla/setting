252e560099eb1f0dfe5ee62ea461fdda7066fda9
cocobolla <pcal1029@gmail.com>
2018-02-10 22:26:22 +0900

    Decorate bash shell

---
+++ dotfiles/bootstrap.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+git pull origin master
+
+git submodule init
+git submodule update
+
+function linking() {
+    [[ -a ~/.$1 ]] && rm -rf ~/.$1
+    ln -s $(pwd)/$1 ~/.$1
+}
+
+for file in {bashrc,vimrc,vim,tmux.conf}; do
+    linking $file;
+done;
+unset file;
+
+unset linking 
